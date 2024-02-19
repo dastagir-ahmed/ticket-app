ceb9e65641194cdfd3fdcf15ffc2a898d60bfefd
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-02-19 13:11:57 +0800

    add fluentui_icons

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,10 +24,22 @@ class _BottomNavBarState extends State<BottomNavBar> {
         unselectedItemColor: const Color(0xFF526400),
         showSelectedLabels: false,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label:"Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label:"Tickets"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label:"Profile"),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+            label: "Home",),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+            label: "Search",),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+            label: "Tickets",),
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+            label: "Profile",),
         ],
 
       ),
