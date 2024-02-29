ffe2de80a310ca99cc3177284d79311f7d0d3223
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-02-29 23:54:38 +0800

    Show and rotate plane icond

--- lib/base/widgets/app_layoutbuilder_widget.dart
+++ lib/base/widgets/app_layoutbuilder_widget.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 
 class AppLayoutBuilderWidget extends StatelessWidget {
   final int randomDivider;
-  
   const AppLayoutBuilderWidget({super.key, required this.randomDivider});
-
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
-      print("${(constraints.constrainWidth()/randomDivider).floor()}");
-      return Flex(
+      return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        direction: Axis.horizontal,
+        //direction: Axis.horizontal,
         children: List.generate((constraints.constrainWidth()/randomDivider).floor(), (index) => const SizedBox(
           width: 3,height:1,child: DecoratedBox(
           decoration: BoxDecoration(
