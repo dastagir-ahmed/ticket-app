b6d9fd0d3c7cef23f5ad8f341f850ba3eabacd39
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-03-02 22:49:09 +0800

    Create big cirlce widget for ticket

--- lib/base/widgets/ticket_view.dart
+++ lib/base/widgets/ticket_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/widgets/app_layoutbuilder_widget.dart';
+import 'package:ticket_app/base/widgets/big_circle.dart';
 import 'package:ticket_app/base/widgets/big_dot.dart';
 
 class TicketView extends StatelessWidget {
@@ -12,7 +13,7 @@ class TicketView extends StatelessWidget {
 
     return SizedBox(
       width: size.width * 0.85,
-      height: 179,
+      height: 189,
       child: Container(
         margin: const EdgeInsets.only(right: 16),
         child: Column(
@@ -86,9 +87,20 @@ class TicketView extends StatelessWidget {
             ),
             Container(
 
+              color: AppStyles.ticketOrange,
+              child:  Row(
+                children: [
+                 BigCircle(isRight: false,),
+                  Expanded(child: Container()),
+                  BigCircle(isRight: true,)
+                ],
+              ),
+            ),
+            Container(
+
               padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  color: Colors.orange,
+              decoration:  BoxDecoration(
+                  color: AppStyles.ticketOrange,
                   borderRadius: const BorderRadius.only(
                       bottomLeft: Radius.circular(21), bottomRight: Radius.circular(21))),
               child: Column(
