baf106320f07f58c96b68a2de2d3709e4d591359
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-03-08 23:18:14 +0800

    Show all tickets in a new route

--- lib/screens/all_tickets.dart
+++ lib/screens/all_tickets.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/utils/all_json.dart';
+import 'package:ticket_app/base/widgets/ticket_view.dart';
 
 class AllTickets extends StatelessWidget {
   const AllTickets({super.key});
@@ -7,7 +9,24 @@ class AllTickets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("All Tickets"),
+      ),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: ticketList
+                  .map((singleTicket) =>
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                          child: TicketView(ticket: singleTicket, wholeScreen: true,)
+                      ))
+                  .toList(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,70 +16,70 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         backgroundColor: AppStyles.bgColor,
         body: ListView(
-      children: [
-        const SizedBox(height:40),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
                 children: [
-                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Good morning",
-                          style: AppStyles.headLineStyle3),
-                      const SizedBox(
-                        height: 5,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Good morning", style: AppStyles.headLineStyle3),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text("Book Tickets", style: AppStyles.headLineStyle1)
+                        ],
                       ),
-                      Text("Book Tickets",
-                          style: AppStyles.headLineStyle1)
+                      Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: const DecorationImage(
+                                  image: AssetImage(AppMedia.logo))))
                     ],
                   ),
+                  const SizedBox(height: 25),
                   Container(
-                    width: 50,
-                    height: 50,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
                     decoration: BoxDecoration(
-                      borderRadius:  BorderRadius.circular(10),
-                      image: const DecorationImage(
-                          image: AssetImage(AppMedia.logo)
-
-                      )
-                    )
-                  )
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD),
+                    ),
+                    child: const Row(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(FluentSystemIcons.ic_fluent_search_regular,
+                            color: Color(0xFFBFC205)),
+                        Text("Search")
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  const AppDoubleText(
+                    bigText: 'Upcoming Flights',
+                    smallText: 'View all',
+                  ),
+                  const SizedBox(height: 20),
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: ticketList
+                            .take(2)
+                            .map((singleTicket) =>
+                                TicketView(ticket: singleTicket))
+                            .toList(),
+                      )),
                 ],
               ),
-              const SizedBox(height:25),
-              Container(
-                padding:const EdgeInsets.symmetric(horizontal:12, vertical:12),
-                decoration: BoxDecoration(
-                    borderRadius:  BorderRadius.circular(10),
-                    color: const Color(0xFFF4F6FD),
-                ),
-                child: const Row(
-                 // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(FluentSystemIcons.ic_fluent_search_regular, color:Color(0xFFBFC205)),
-                    Text("Search")
-                  ],
-                ),
-              ),
-              const SizedBox(height:40),
-              const AppDoubleText(bigText: 'Upcoming Flights', smallText: 'View all',),
-              const SizedBox(height:20),
-              SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: ticketList.take(2).map((singleTicket) =>  TicketView(ticket:singleTicket)
-                    ).toList(),
-                  )
-              ),
-            ],
-          ),
-        ),
-
-      ],
-    ));
+            ),
+          ],
+        ));
   }
 }
