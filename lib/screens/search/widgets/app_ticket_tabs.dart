5018d61a69196d8ee13381e59230c75f754f9b15
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-03-20 22:46:18 +0800

    TicketScreen and Tabs

--- lib/screens/search/widgets/app_ticket_tabs.dart
+++ lib/screens/search/widgets/app_ticket_tabs.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class AppTicketTabs extends StatelessWidget {
-  const AppTicketTabs({super.key});
+  final String firstTab;
+  final String secondTab;
+  const AppTicketTabs({super.key, required this.firstTab, required this.secondTab});
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +12,12 @@ class AppTicketTabs extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50),
           color: AppStyles.ticketTabColor),
-      child: const Row(children: [
+      child:  Row(children: [
         AppTabs(
-          tabText: "All tickets",
+          tabText: firstTab,
         ),
         AppTabs(
-          tabText: "Hotels",
+          tabText: secondTab,
           tabBorder: true,
           tabColor: true,
         )
