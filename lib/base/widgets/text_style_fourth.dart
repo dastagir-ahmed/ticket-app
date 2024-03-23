2c0eee4bad3af6afd89b790e1c6b94fc95c220d2
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-03-24 06:57:49 +0800

    Change bottom text color

--- lib/base/widgets/text_style_fourth.dart
+++ lib/base/widgets/text_style_fourth.dart
@@ -7,14 +7,19 @@ class TextStyleFourth extends StatelessWidget {
   final bool? isColor;
 
   const TextStyleFourth(
-      {super.key, required this.text, this.align = TextAlign.start, this.isColor});
+      {super.key,
+      required this.text,
+      this.align = TextAlign.start,
+      this.isColor});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       textAlign: align,
-      style: isColor==null? AppStyles.headLineStyle4.copyWith(color: Colors.white):AppStyles.headLineStyle4,
+      style: isColor == null
+          ? AppStyles.headLineStyle4.copyWith(color: Colors.white)
+          : AppStyles.headLineStyle4,
     );
   }
 }
