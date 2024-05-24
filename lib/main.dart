bb6a08a43645460757c555d63b5f7ac6f56a5b79
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-05-24 11:25:15 +0800

    added routing for hotels

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ticket_app/base/bottom_nav_bar.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/app_routes.dart';
+import 'package:ticket_app/screens/home/all_hotels.dart';
 import 'package:ticket_app/screens/home/all_tickets.dart';
 import 'package:ticket_app/screens/ticket/ticket_screen.dart';
 
@@ -22,7 +23,8 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.homePage:(context)=> const BottomNavBar(),
         AppRoutes.allTickets:(context)=> const AllTickets(),
-        AppRoutes.ticketScreen:(context)=> const TicketScreen()
+        AppRoutes.ticketScreen:(context)=> const TicketScreen(),
+        AppRoutes.allHotels:(context)=> const AllHotels()
       },
     );
   }
---
+++ lib/screens/home/all_hotels.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+
+class AllHotels extends StatelessWidget {
+  const AllHotels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      appBar: AppBar(
+        title: Text("All Hotels"),
+        backgroundColor: AppStyles.bgColor,
+      ),
+    );
+  }
+}
