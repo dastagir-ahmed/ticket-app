40c6312e530cc046e737f59a8a34a750ba2a762f
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-05-25 23:44:26 +0800

    added hotel routing with view

--- lib/screens/home/all_hotels.dart
+++ lib/screens/home/all_hotels.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
 import 'package:ticket_app/screens/home/widgets/hotel.dart';
@@ -22,15 +23,84 @@ class AllHotels extends StatelessWidget {
                 crossAxisCount: 2,
                 crossAxisSpacing: 16.0,
               mainAxisSpacing: 16.0,
-              childAspectRatio: 1.0
+              childAspectRatio: 0.9
 
             ),
             itemCount: hotelList.length,
             itemBuilder: (context, index){
               var singleHotel = hotelList[index];
-              return Hotel(hotel: singleHotel);
+              return HotelGridView(hotel: singleHotel);
             }
         ),
+      ),
+    );
+  }
+}
+
+class HotelGridView extends StatelessWidget {
+  final Map<String,dynamic> hotel;
+  const HotelGridView({super.key, required this.hotel});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      //width: size.width*0.6,
+      height: 350,
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+          color: AppStyles.primaryColor,
+          borderRadius: BorderRadius.circular(18)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 1.2,
+            child: Container(
+
+              decoration: BoxDecoration(
+                  color: AppStyles.primaryColor,
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                      fit:BoxFit.cover,
+                      image: AssetImage(
+                          "assets/images/${hotel['image']}"
+                      )
+                  )
+              ),
+            ),
+          ),
+          const SizedBox(height: 5,),
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Text(
+              hotel['place'],
+              style: AppStyles.headLineStyle3.copyWith(color:AppStyles.kakiColor),
+            ),
+          ),
+
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Text(
+                  hotel['destination'],
+                  style: AppStyles.headLineStyle3.copyWith(color:Colors.white),
+                ),
+              ),
+              SizedBox(width: 5,),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Text(
+                  "\$${hotel['price']}/night",
+                  style: AppStyles.headLineStyle4.copyWith(color:AppStyles.kakiColor),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -23,9 +23,12 @@ class _TicketScreenState extends State<TicketScreen> {
 
   @override
   void didChangeDependencies() {
-    var args =  ModalRoute.of(context)!.settings.arguments as Map;
-    print("passed index ${args["index"]}");
-    ticketIndex = args["index"];
+    if(ModalRoute.of(context)!.settings.arguments!=null){
+      var args =  ModalRoute.of(context)!.settings.arguments as Map;
+      print("passed index ${args["index"]}");
+      ticketIndex = args["index"];
+    }
+
     super.didChangeDependencies();
   }
 
