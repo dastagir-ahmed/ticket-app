7d4c2a5331498430045ed8fc37908190e293e9a6
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-05-30 11:22:37 +0800

    leading backbutton using icon

--- lib/screens/hotel_detail.dart
+++ lib/screens/hotel_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
 
 class HotelDetail extends StatefulWidget {
@@ -29,6 +30,24 @@ class _HotelDetailState extends State<HotelDetail> {
             expandedHeight: 300.0,
             floating: false,
             pinned: true,
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppStyles.primaryColor
+                  ),
+                  child: Icon(
+                      Icons.arrow_back,
+                      color:Colors.white
+                  ),
+                ),
+              ),
+            ),
             flexibleSpace: FlexibleSpaceBar(
               title: Text(hotelList[index]["place"]),
               background: Image.asset("assets/images/${hotelList[index]["image"]}"),
@@ -36,31 +55,31 @@ class _HotelDetailState extends State<HotelDetail> {
           ),
           SliverList(
               delegate: SliverChildListDelegate([
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                  "In this article, we will create a custom scrollable app bar with a background image in Flutter. The app bar will shrink as the user scrolls up, and it will include a back button. Below the image, we will display some descriptive text with a More or Less button to show or hide additional text. Finally, we will add a section to display recent images in a horizontal list."),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "More Images",
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                )),
-            Container(
-              height: 200.0,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Container(
-                        margin: EdgeInsets.all(16),
-                        color: Colors.red,
-                        child: Image.network(
-                            "https://via.placeholder.com/200x200"));
-                  }),
-            )
-          ]))
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                      "In this article, we will create a custom scrollable app bar with a background image in Flutter. The app bar will shrink as the user scrolls up, and it will include a back button. Below the image, we will display some descriptive text with a More or Less button to show or hide additional text. Finally, we will add a section to display recent images in a horizontal list."),
+                ),
+                const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "More Images",
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    )),
+                Container(
+                  height: 200.0,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            margin: EdgeInsets.all(16),
+                            color: Colors.red,
+                            child: Image.network(
+                                "https://via.placeholder.com/200x200"));
+                      }),
+                )
+              ]))
         ],
       ),
     );
